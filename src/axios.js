20a05f38e6c882776e85b35e5b05968503b9eb77
facudelucia <facundodeluciajudo@gmail.com>
2020-12-07 15:21:20 -0300

    cambio url backend

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const instance = axios.create({
-    baseURL:"https://tinder-backend-facu.herokuapp.com"
+    baseURL:"https://fantasy-solstice-touch.glitch.me/"
 })
 
 export default instance
